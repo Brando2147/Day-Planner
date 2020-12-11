31d69555c122d89c7177c6105eb872775aaaec2f
Brando D <brando2147@gmail.com>
2020-12-10 20:11:20 -0700

    Initial commit

---
+++ Develop/Script.js
@@ -0,0 +1,18 @@
+// Declaring global variables 
+let hrNine = $("hrNine")
+let hrTen = $("hrTen")
+let hrEleven = $("hrEleven")
+let hrTwelve = $("hrTwelve")
+let hrOne = $("hrOne")
+let hrTwo = $("hrTwo")
+let hrThree = $("hrThree")
+let hrFour = $("hrFour")
+let hrFive = $("hrFive")
+let saveBtn = $("saveBtn")
+let textArea = $("textarea")
+let currentDay = $("#currentDay")
+let jumboTron = $("jumbotron")
+
+
+currentDay.text(moment().format(""));
+currentTime.text(moment().format(""));
