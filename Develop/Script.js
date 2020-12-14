b0dce97f5f0b40eaeb212fc5d8a202112c330d57
Brando D <brando2147@gmail.com>
2020-12-14 00:06:04 -0700

    updating logic for timeblock styles

--- Develop/Script.js
+++ Develop/Script.js
@@ -1,18 +1,79 @@
-// Declaring global variables 
-let hrNine = $("hrNine")
-let hrTen = $("hrTen")
-let hrEleven = $("hrEleven")
-let hrTwelve = $("hrTwelve")
-let hrOne = $("hrOne")
-let hrTwo = $("hrTwo")
-let hrThree = $("hrThree")
-let hrFour = $("hrFour")
-let hrFive = $("hrFive")
-let saveBtn = $("saveBtn")
-let textArea = $("textarea")
-let currentDay = $("#currentDay")
-let jumboTron = $("jumbotron")
+jQuery(document).ready(function () {
 
 
-currentDay.text(moment().format(""));
-currentTime.text(moment().format(""));
+    // // Declaring global variables 
+    // let hrNine = $("hrNine")
+    // let hrTen = $("hrTen")
+    // let hrEleven = $("hrEleven")
+    // let hrTwelve = $("hrTwelve")
+    // let hrOne = $("hrOne")
+    // let hrTwo = $("hrTwo")
+    // let hrThree = $("hrThree")
+    // let hrFour = $("hrFour")
+    // let hrFive = $("hrFive")
+    // let saveBtn = $("saveBtn")
+    let textArea = $("textarea")
+    // let jumboTron = $("jumbotron")
+    let currentDate = luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' });;
+    $("#currentDay").text(currentDate);
+    let currentTime = luxon.DateTime.local().get('hour');
+    var currentTimeInt = parseInt(currentTime);
+
+
+    // let hoursArr = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+
+    // let events = {
+    //     hr9: textArea[0].value.trim(),
+    //     hr10: textArea[1].value.trim(),
+    //     hr11: textArea[2].value.trim(),
+    //     hr12: textArea[3].value.trim(),
+    //     hr1: textArea[4].value.trim(),
+    //     hr2: textArea[5].value.trim(),
+    //     hr3: textArea[6].value.trim(),
+    //     hr4: textArea[7].value.trim(),
+    //     hr5: textArea[8].value.trim(),
+    // }
+
+
+
+    console.log(currentTime)
+    // for (let i = 0; i < hoursArr.length; i++) {
+
+    //     let hrBlock = hoursArr[i];
+    //     let newRow = rows[i];
+
+    $(".time-block").each(function () {
+        let hrBlock = this.id
+        // currentTime = parseInt(currentTime);
+
+        if (hrBlock === currentTimeInt) {
+            $(this).find('textarea').addClass('present')
+        }
+
+        else if (hrBlock > currentTimeInt) {
+            $(this).find('textarea').addClass('future')
+        }
+
+        else if (hrBlock < currentTimeInt) {
+            $(this).find('textarea').addClass('past')
+        }
+        console.log(currentTimeInt)
+    });
+
+
+
+
+
+    // $('.saveBtn').on('click', saveEvents);
+
+    // Add event listener for save button  
+    // function saveEvents() {
+
+    //     localStorage.setItem("", count);
+
+
+
+    // }
+
+
+})
